403c368b51da4296aa743645bd65eaa455231a3c
M Wong <wongmt15@gmail.com>
2024-09-05 00:41:35 +1000

    5-9-24

--- app/cv/page.js
+++ app/cv/page.js
@@ -21,19 +21,19 @@ export default function CV() {
 	<li>M.Sc in Electronic Engineering</li>
 	<li>B.Sc (Hons.) in Electrical & Electronic Engineering</li>
 	</ul>
-	
+	<br />
 	<h3>Certificates</h3>
 	<ul>
 	<li>IBM Data Science Professional Certificate by IBM on Coursera</li>
 	<li>Professional Certificate of "DeepLearning.AI TensorFlow Developer", by DeepLearning.AI on Coursera</li>
 	</ul>
-		
+	<br />	
 	<h3>Research specialism</h3>
 	<ul>
 	<li>Computer vision</li>
 	<li>Machine learning</li>
 	</ul>
-
+    <br />
 	<h3>Skills & experience</h3>
 	<ul>
 	<li>Programming languages: Python, Java, JavaScript, C++</li>
@@ -41,8 +41,7 @@ export default function CV() {
 	<li>Machine learning library: Tensorflow, scikit-learn, Weka</li>
 	<li>Computer vision library: OpenCV, scikit-image</li>
 	</ul>
-	</div>
-	  
+	</div>	  
     </>
   );
 }
--- app/posts/post3/page.js
+++ app/posts/post3/page.js
@@ -10,7 +10,7 @@ export default function Page() {
       <br />
 	  <h3>Title: Software update</h3>
 	  <p>Date: 4-9-2024</p>
-	  <p>This site has been updated with Next.js 14</p>	  
+	  <p>This site has been updated with Next.js 14.</p>	  
     </>
   );
 }
