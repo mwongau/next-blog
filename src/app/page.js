8965b4de5289e8b74f0ad9b011af6fee4ea544be
M Wong <wongmt15@gmail.com>
2025-12-12 21:56:31 +1100

    12-12-25

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div>  
 	  <h2>Home</h2>	  
 	  <br />
-	  <p>Welcome to the personal site of M. Wong. I am a researcher in computer vision and 
+	  <p>Welcome to the personal site of M Wong. I am a researcher in computer vision and 
 	  machine learning with Ph.D in Computer Science, M.Sc and B.Sc(Hons.) in Electrical and 
 	  Electronic Engineering. I am also interested in web app development. This site is built 
 	  by using Next.js, React.js, JavaScript, HTML and CSS. Hosting is done by Vercel.</p>
